bb79548f20a181c3fb2370f523f7e664e532a9fc
bhumi110 <bhumisaraogi@gmail.com>
2025-12-15 20:01:32 +0530

    deployment error fix

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,11 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.status(200).send("FoodFlow API is running 🚀");
+});
+
+
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
