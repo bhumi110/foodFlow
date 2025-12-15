854ab35e61b089cea306fe261825c47936557958
bhumi110 <bhumisaraogi@gmail.com>
2025-12-15 20:11:20 +0530

    deployment error fix

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173","https://foodflow-uw8o.onrender.com","*"],
+  origin: ["http://localhost:5173","https://foodflow-lzfy.onrender.com"],
   credentials: true,
 }));
 app.use(express.json());
